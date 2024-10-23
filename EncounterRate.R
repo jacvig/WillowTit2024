042d58d01e091c4c66d7ea83bc44d3cec2e9d443
Jacqueline Vigilanti <jacqueline.vigilanti@gmail.com>
2024-10-23 15:34:54 +0100

    Updated with random forest and calibration code

--- EncounterRate.R
+++ EncounterRate.R
@@ -64,7 +64,31 @@ ne_state_lines <- read_sf("data/gis-data.gpkg", "ne_state_lines") |>
 checklists_ss <- grid_sample_stratified(checklists_env,
                                         obs_column = "species_observed",
                                         sample_by = "type")
-  ## subsampling has reduced the number of rows by 174,150 (65%).
+  ## Check how much subsampling has reduced the number of rows.
+  # original data
+    nrow(checklists_env)
+    #> [1] 269775
+    count(checklists_env, species_observed) |> 
+      mutate(percent = n / sum(n))
+    #> # A tibble: 2 × 3
+    #>   species_observed     n percent
+    #>   <lgl>            <int>   <dbl>
+    #>  1 FALSE            268319 0.995  
+    #>  2 TRUE               1456 0.00540
+
+    # after sampling
+    nrow(checklists_ss)
+    #> [1] 95625
+    count(checklists_ss, species_observed) |> 
+      mutate(percent = n / sum(n))
+    #> # A tibble: 2 × 3
+    #>   species_observed     n percent
+    #>   <lgl>            <int>   <dbl>
+    #>  1 FALSE            94690 0.990  
+    #>  2 TRUE               935 0.00978      # subsampling decreased the overall number of checklists by a factor of 2.8 but increased the prevalence of detections from .5% to .9% 
+
+
+
 
 
 # convert checklists to spatial features
@@ -114,3 +138,81 @@ for (i in seq_along(both_pts)) {
   }
 }
 par(p)
+
+
+
+# Random forest
+  ## Will use a balanced forest model 
+
+# filter to training set
+checklists_train <- checklists_ss |> 
+  filter(type == "train") |> 
+  # select only the columns to be used in the model
+  select(species_observed,
+         year, day_of_year, hours_of_day,
+         effort_hours, effort_distance_km, effort_speed_kmph,
+         number_observers, 
+         starts_with("pland_"),
+         starts_with("ed_"),
+         starts_with("elevation_"))
+
+
+# calculate detection frequency
+detection_freq <- mean(checklists_train$species_observed)
+
+# ranger requires a factor response to do classification
+er_model <- ranger(formula =  as.factor(species_observed) ~ ., 
+                   data = checklists_train,
+                   importance = "impurity",
+                   probability = TRUE,
+                   replace = TRUE, 
+                   sample.fraction = c(detection_freq, detection_freq))
+
+
+# Calibration
+
+# predicted encounter rate based on out of bag samples
+er_pred <- er_model$predictions[, 2]
+# observed detection, converted back from factor
+det_obs <- as.integer(checklists_train$species_observed)
+# construct a data frame to train the scam model
+obs_pred <- data.frame(obs = det_obs, pred = er_pred)
+
+# train calibration model
+calibration_model <- scam(obs ~ s(pred, k = 6, bs = "mpi"), 
+                          gamma = 2,
+                          data = obs_pred)
+
+
+# group the predicted encounter rate into bins of width 0.02
+# then calculate the mean observed encounter rates in each bin
+er_breaks <- seq(0, 1, by = 0.02)
+mean_er <- obs_pred |>
+  mutate(er_bin = cut(pred, breaks = er_breaks, include.lowest = TRUE)) |>
+  group_by(er_bin) |>
+  summarise(n_checklists = n(),
+            pred = mean(pred), 
+            obs = mean(obs),
+            .groups = "drop")
+
+# make predictions from the calibration model
+calibration_curve <- data.frame(pred = er_breaks)
+cal_pred <- predict(calibration_model, calibration_curve, type = "response")
+calibration_curve$calibrated <- cal_pred
+
+# compared binned mean encounter rates to calibration model
+ggplot(calibration_curve) +
+  aes(x = pred, y = calibrated) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
+  geom_line(color = "blue") +
+  geom_point(data = mean_er, 
+             aes(x = pred, y = obs),
+             size = 2, alpha = 0.6,
+             show.legend = FALSE) +
+  labs(x = "Estimated encounter rate",
+       y = "Observed encounter rate",
+       title = "Calibration model") +
+  coord_equal(xlim = c(0, 1), ylim = c(0, 1))
+
+  ## The estimated encounter rates are larger than the observed encounter rates: All points fall below the x-y line, i.e. it is not well calibrated
+  ## though relative ranking is good: points with higher estimated rates also have higher observed rates.
